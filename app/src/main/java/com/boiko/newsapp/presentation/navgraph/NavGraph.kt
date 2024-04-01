9315bf96de31bbfe8d028b529c9611d6c63ec028
IvanBoik <boiko12332@gmail.com>
2024-04-01 23:11:36 +0400

    Finished home screen

--- app/src/main/java/com/boiko/newsapp/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/boiko/newsapp/presentation/navgraph/NavGraph.kt
@@ -2,7 +2,6 @@ package com.boiko.newsapp.presentation.navgraph
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,6 +10,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.boiko.newsapp.presentation.home.HomeScreen
+import com.boiko.newsapp.presentation.home.HomeViewModel
 import com.boiko.newsapp.presentation.onboarding.OnBoardingScreen
 import com.boiko.newsapp.presentation.onboarding.OnBoardingViewModel
 
@@ -42,9 +44,12 @@ fun NavGraph(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    Text(text = "News navigation Screen")
+                    val viewModel: HomeViewModel = hiltViewModel()
+                    val articles = viewModel.news.collectAsLazyPagingItems()
+                    HomeScreen(articles = articles) {
+                        
+                    }
                 }
-
             }
         }
     }
